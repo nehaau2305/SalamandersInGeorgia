f64f985bf44be48200b9a14e4b7c67dd673f9394
nehaau2305 <nehaau2305@gmail.com>
2025-10-09 18:50:01 -0400

    SalamanderActivity.java basic setup with action bar to navigate to previous page/fragment

--- app/src/main/java/projects/mobile/nehaa/salamandersingeorgia/SalamanderActivity.java
+++ app/src/main/java/projects/mobile/nehaa/salamandersingeorgia/SalamanderActivity.java
@@ -1,4 +1,33 @@
 package projects.mobile.nehaa.salamandersingeorgia;
 
-public class SalamanderActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SalamanderActivity extends AppCompatActivity {
+    // initialize variable
+    private int selectedIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_salamander);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // action bar to navigate to previous page using back button
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    } //onCreate
 }
